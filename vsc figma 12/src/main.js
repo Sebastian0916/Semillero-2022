510bd8cb4b17d419213ff11d50751c01206f212e
Sebastian <Sebastian Vera>
2022-06-07 17:45:11 -0500

    js308

---
+++ vsc figma 12/src/main.js
@@ -0,0 +1,19 @@
+function bolita() {
+  const cero = document.getElementsByClassName("bola")[0];
+  cero.classList.toggle("hidde");
+}
+let arrayPlayData = [];
+fetch("./src/playData.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then(  function (jsondata) {
+    for(let prop in jsondata){
+         let opt = document.createElement("option");
+        opt.value = prop;
+        let textOpt = document.createTextNode(jsondata[prop]);
+        opt.appendChild(textOpt);
+      document.getElementById("moneda").appendChild(opt);
+      }
+  }) 
+      
